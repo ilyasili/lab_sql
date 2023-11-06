9d471287af27159fda465cbbb116e5ce4a57c4bf
ilyasili <147747052+ilyasili@users.noreply.github.com>
2023-11-06 22:56:02 +0200

    Add files via upload

--- lab1.sql
+++ lab1.sql
@@ -1,22 +1,20 @@
-1
-
-SELECT name_city
+-1/
+SELECT * 
 FROM cities
-WHERE population > 1000000;
-
-2
-  
-SELECT * FROM cities WHERE region IN("E","W") ORDER BY population
-
-3
-
-SELECT * FROM cities WHERE region IN("S","C","N") AND population >= 50000
-
-4
-  
+ORDER BY population DESC
+LIMIT 10 OFFSET 10;
+2/
+SELECT *
+FROM cities
+ORDER BY name DESC
+LIMIT 30 OFFSET 0
+3/
+SELECT *
+FROM cities 
+ORDER BY region, population 
+4/
+SELECT region
+FROM cities
+5/
 SELECT * FROM cities
-  WHERE region IN("E","W","N") AND population BETWEEN 150000 AND 350000 LIMIT 20;
-
-5
-
-SELECT * FROM cities WHERE region NOT IN("E","W") LIMIT 20;
+ORDER BY region DESC, name DESC;
